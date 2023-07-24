4137a332d8acd569175795609fa7b03de2d5f93c
Chohyeon Min <70613905+angly97@users.noreply.github.com>
2023-07-24 10:02:09 +0900

    [Gold IV] Title: 좋다, Time: 168 ms, Memory: 12820 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -14,15 +14,19 @@ public class Main {
 		for(int i=0; i<n; i++)
 			arr[i] = Integer.parseInt(st.nextToken());
 		
+		
+		// 투포인터 사용을 위해 arr 오름차순 정렬
 		Arrays.sort(arr);
 		
 		int cnt = 0, sum = 0, right;
 		
 		for(int i=0; i<n; i++) {
 			
+			// left = 0, right = n-1부터 가운데로 모이면서 투포인터 수행
 			right = n-1;
 			for(int left = 0; left<right; left++) {
 				
+				// 자기자신(i) 제외
 				if(i == left) continue;
 				else if(i == right) {
 					right--;
@@ -38,8 +42,9 @@ public class Main {
 					right--;
 				}
 				
+				// 자기 자신을 제외한 원소들로 합을 만들 수 있음
 				if(sum == arr[i]) {
-    				cnt++;
+					cnt++;
 					break;
 				}
 			}
