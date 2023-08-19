ca25ee3bcbb2e0084a8bffde1b4bbf653686b2eb
Chohyeon Min <70613905+angly97@users.noreply.github.com>
2023-08-19 11:08:09 +0900

    [Gold IV] Title: 마법사 상어와 파이어볼, Time: 364 ms, Memory: 35276 KB -BaekjoonHub

--- 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.java
+++ 백준/Gold/20056. 마법사 상어와 파이어볼/마법사 상어와 파이어볼.java
@@ -4,6 +4,7 @@ import java.io.*;
 /*
  * 한번에 이동하는 거리를 n으로 모듈러한 것을 받아서 틀림.
  * 앞으로 처음 받을때부터 모듈러로 받지 말자!!
+ * 다음 행 =  ( 현재행 + 거리*방향%n + n)%n
  */
 public class Main {
 	public static void main(String[] args) throws Exception {
@@ -18,23 +19,24 @@ public class Main {
 		// fires : 파이어볼 큐
 		Queue<int[]> fires = new ArrayDeque<>();
 		
-		// fireQueues[i][j] : (i, j)에 있는 파이어볼들을 큐에 담고 있음
-		Queue<int[]>[][] fireQueues = new ArrayDeque[n][n];
+		// map[i][j] : (i, j)에 있는 파이어볼들을 큐에 담고 있음
+		Queue<int[]>[][] map = new ArrayDeque[n][n];
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<n; j++)
-				fireQueues[i][j] = new ArrayDeque<>();
+				map[i][j] = new ArrayDeque<>();
 		}
 		
-		int r, c, M, s, d;
+		// 파이어볼 정보 입력 받기
 		for(int i=0; i<m; i++) {
 			st = new StringTokenizer(in.readLine());
-			r = Integer.parseInt(st.nextToken())-1;
-			c = Integer.parseInt(st.nextToken())-1;
-			M = Integer.parseInt(st.nextToken());
-			s = Integer.parseInt(st.nextToken());
-			d = Integer.parseInt(st.nextToken());
 			
-			fires.add(new int[] {r, c, M, s, d});
+			fires.add(new int[] {
+					Integer.parseInt(st.nextToken())-1,
+					Integer.parseInt(st.nextToken())-1,
+					Integer.parseInt(st.nextToken()),
+					Integer.parseInt(st.nextToken()),
+					Integer.parseInt(st.nextToken())
+				});
 		}
 		
 		
@@ -51,36 +53,30 @@ public class Main {
 		// k번 이동 명령
 		for(int round=0; round<k; round++) {
 			
-			// 1. 파이어볼 이동
-//			System.out.println("-------------------------");
-//			System.out.println("불 이동");
-			
+			// 1. 파이어볼 이동 : 파이어볼을 큐에서 뽑아내면서, 이동시키고 그 결과는 map에 저장
 			while(!fires.isEmpty()) {
 				nowFire = fires.poll();
-//				System.out.print("("+nowFire[0]+", "+nowFire[1]+") --> ");
 				
-				nxtr = (nowFire[0] + (nowFire[3]*deltas[nowFire[4]][0])% n + n)%n;
-				nxtc = (nowFire[1] + (nowFire[3]*deltas[nowFire[4]][1])% n + n)%n;
+				nxtr = (nowFire[0] + nowFire[3] * deltas[nowFire[4]][0] % n + n)%n;
+				nxtc = (nowFire[1] + nowFire[3] * deltas[nowFire[4]][1] % n + n)%n;
 				
-				fireQueues[nxtr][nxtc].offer(new int[] {nowFire[2], nowFire[3], nowFire[4]});
-//				System.out.println("("+nxtr+", "+nxtc+")");
+				map[nxtr][nxtc].offer(new int[] {nowFire[2], nowFire[3], nowFire[4]});
 			}
 
-
-//			System.out.println("2개 이상인 칸 4개의 불로 분리");
-			// 파이어볼 2개 이상인 칸은 4개로 분리됨
+			
+			// 2. 파이어볼 2개 이상인 칸은 4개로 분리됨
 			for(int i=0; i<n; i++) {
 				for(int j=0; j<n; j++) {
-					if(fireQueues[i][j].size() == 1) {
-						nowFire = fireQueues[i][j].poll();
+					
+					if(map[i][j].size() == 1) {			// (i, j)에 파이어볼 1개 있으면 나누지않고, 다음 fires에 추가 
+						nowFire = map[i][j].poll();
 						fires.offer(new int[] {i, j, nowFire[0], nowFire[1], nowFire[2]});
 					}
-					else if(fireQueues[i][j].size() >= 2) {
+					
+					else if(map[i][j].size() >= 2) {	// (i, j)에 있는 파이어볼을 4개로 나누기
 						
-						fireQ = fireQueues[i][j];		// (i, j)에 있는 파이어볼 큐
+						fireQ = map[i][j];				// (i, j)에 있는 파이어볼 큐
 						fireCnt = fireQ.size();			// (i, j)에 있는 파이어볼 갯수
-
-//						System.out.println("("+i+", "+j+") 칸의 불 갯수: "+fireCnt);
 						
 						// 현재 칸의 질량합, 속력합, 방향%2합 초기화
 						sumM = sumS = sumD = 0;
@@ -88,15 +84,11 @@ public class Main {
 						// fireQ에 있는 파이어볼들의 질량 합, 속력 합, 방향%2의 합 계산
 						while(!fireQ.isEmpty()) {
 							nowFire = fireQ.poll();
-//							System.out.println(Arrays.toString(nowFire));
 
 							sumM += nowFire[0];
 							sumS += nowFire[1];
 							sumD += nowFire[2]%2;
 						}
-						
-//						System.out.println("("+i+", "+j+") 칸의 질량,속력,방향 합: "+sumM+" "+sumS+" "+sumD);
-
 						
 						// 4개로 나뉠 파이어볼들의 질량과 속력 갱신
 						renewM = sumM/5;
@@ -111,7 +103,7 @@ public class Main {
 							// 4개로 나뉜 파이어볼의 방향들이 각각 0 2 4 6
 							for(int renewD=0; renewD<=6; renewD+=2) {
 								
-								// 1번 연산에 쓰일 파이어볼 큐에 담기
+								// fires에 담기
 								fires.offer(new int[] {i, j, renewM, renewS, renewD});
 							}
 						}
@@ -119,41 +111,21 @@ public class Main {
 							// 4개로 나뉜 파이어볼의 방향들이 각각 1 3 5 7
 							for(int renewD=1; renewD<=7; renewD+=2) {
 								
-								// 1번 연산에 쓰일 파이어볼 큐에 담기
+								// fires에 담기
 								fires.offer(new int[] {i, j, renewM, renewS, renewD});
 							}
 						}
-
-//						System.out.println("현재 칸 불의 갯수: "+fireQ.size());
-//						System.out.println("현재 칸 불이 나뉜 결과 불의 갯수: "+fires.size());
 					}
 				}
 			}
-			
-			
-//			System.out.println("연산 결과 ");
-//			for(int i=0; i<n; i++)
-//				for(int j=0; j<n; j++) {
-//					size=  fireQueues[i][j].size();
-//					if(size > 0)
-//						System.out.println(i+" "+j);
-//					while(size-->0) {
-//						nowFire = fireQueues[i][j].poll();
-//						System.out.println(Arrays.toString(nowFire));
-//						fireQueues[i][j].offer(nowFire);
-//					}
-//				}
 		}
 		
 
 		int ans = 0;
 
-//		System.out.println("남은 질량 구하기");
-		
 		// 남은 질량 구하기
 		while(!fires.isEmpty()) {
 			nowFire = fires.poll();
-//			System.out.println(Arrays.toString(nowFire));
 			ans += nowFire[2];
 		}
 		
